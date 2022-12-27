27987c7016800990237b14dcb7be468d2453bd45
RogerJung <engineer54rj@gmail.com>
2022-12-27 23:03:44 +0800

    init: word_split.py

---
+++ src/word_split.py
@@ -0,0 +1,54 @@
+import json
+from pathlib import Path
+import pandas as pd
+
+# Opening JSON file
+f = open('../data/2000.json')
+
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+
+
+## data cleaning
+for i, post in enumerate(data):
+    for symbol in ['[',']','!','?','(',')','...','"','“','”','…','_','..','-']:
+        post['title'] = post['title'].replace(symbol, '')
+
+# Iterating through the json
+# list
+
+title = []
+index = []
+body = []
+
+for i, post in enumerate(data):
+    out1 = '' ; out2 = ''
+    sum = 0
+
+    body.append(post['body'])
+    for j, word in enumerate(post['title'].split(' ')):
+        out1 += word + ' '
+        if len(str(j)) > len(word):
+            out1 += ' ' * (len(str(j)) - len(word))
+        out2 += (f'{j:<{len(word)}}') + ' '
+
+    print(f'{out1}\n{out2}')
+    title.append(out1)
+    index.append(out2)
+
+result = []
+for i, post in enumerate(data):
+    result.append([title[i],index[i],body[i]])
+
+posts = pd.DataFrame(result,columns=['title','index','body'])
+result = posts.to_json(orient="records")
+parsed = json.loads(result)
+
+json_object = json.dumps(parsed, indent=4, ensure_ascii=False)
+
+outfile = Path('../data/movies.json')
+outfile.write_text(json_object, encoding='UTF-8')
+
+# Closing file
+f.close()
