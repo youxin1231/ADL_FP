92c2988f60293c0fc1f2503742ac504fa5a02fc3
youxin1231 <tommy135721@gmail.com>
2023-01-03 21:22:01 +0800

    src: update preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,7 @@
 import json
 from pathlib import Path 
+import ast
+from random import sample
 
 def main():
     # Opening JSON file
@@ -13,14 +15,25 @@ def main():
 
     for post in data:
         title = post['title'][:-1]
-        label = post['label']
-        if label is not None:
-            load_data.append(f'{title}####{label}')
+        label_text = post['label']
+        if label_text is not None:
+            labels = ast.literal_eval(label_text)
+            POS = []; NEG = []; NEU = []
+            for label in labels:
+                if label[2] == 'POS':
+                    POS.append(tuple(label[0]))
+                elif label[2] == 'NEG':
+                    NEG.append(tuple(label[0]))
+                elif label[2] == 'NEU':
+                    NEU.append(tuple(label[0]))
+            if not set(POS) & set(NEG) and not set(POS) & set(NEU) and not set(NEG) & set(NEU)  :
+                load_data.append(f'{title}####{label_text}')
 
-    train_data = load_data[:int(0.8*len(load_data))]
-    eval_data = load_data[int(0.8*len(load_data))+1:]
+    train_data = sample(load_data, int(0.8*0.7*len(load_data)))
+    eval_data = sample(load_data, int(0.8*0.3*len(load_data)))
+    test_data = sample(load_data, int(0.2*len(load_data)))
     
-    f = open('./processed_data/train.txt', 'w')
+    f = open('./processed_data/train_triplets.txt', 'w')
     for i in train_data:
         if i == train_data[len(train_data)-1]:
             f.write(f'{i}')
@@ -29,9 +42,18 @@ def main():
 
     f.close()
 
-    f = open('./processed_data/eval.txt', 'w')
+    f = open('./processed_data/dev_triplets.txt', 'w')
     for i in eval_data:
         if i == eval_data[len(eval_data)-1]:
+            f.write(f'{i}')
+        else:
+            f.write(f'{i}\n')
+
+    f.close()
+
+    f = open('./processed_data/test_triplets.txt', 'w')
+    for i in test_data:
+        if i == test_data[len(test_data)-1]:
             f.write(f'{i}')
         else:
             f.write(f'{i}\n')
