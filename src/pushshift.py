eaaed7b031eb76910afbbe95f15ec3aeb562892e
youxin1231 <tommy135721@gmail.com>
2023-01-03 21:21:07 +0800

    crawler: add subreddit input

--- src/pushshift.py
+++ src/pushshift.py
@@ -5,23 +5,31 @@ import datetime as dt
 start_time = dt.datetime.now()
 
 def main():
+    sub = input('Subreddit: ')
+
     dataset_size = 10000
+
+    print('Connecting to pushshift server...')
 
     reddit = praw.Reddit(client_id='yZn7KcD7GMoerEt8tuoF_g',
                         client_secret='JuTvsef5FhiKOGmVgUAmGXIHZxhimQ',
                         user_agent='ADL_project')
     api = PushshiftAPI(reddit)
+    # api = PushshiftAPI()
+
+    print('Finish !')
 
     # start_epoch=int(dt.datetime(2022, 1, 1).timestamp())
     # end_epoch = int(dt.datetime(2022, 12, 25).timestamp())
 
-    movies_list = []
-    
+    post_list = []
+
+    print('Crawling...')
     submissions = api.search_submissions(
                                         # after=start_epoch,
                                         # before=end_epoch,
                                          size=500,
-                                         subreddit='movies',
+                                         subreddit=sub,
                                          fields='title,selftext')
 
     craw_cnt = 0; result_cnt = 0
@@ -33,8 +41,10 @@ def main():
         print(f'{craw_cnt}: {title}\n{body}')
 
         # If post body length >= 64, Add the post to dataset.
+
         if len(title) >= 20 and len(body) >= 64:
             # Preprocessing title, body.
+
             if not 'http' in body:
                 title = title.replace('\n', ' ')
                 body = body.replace('\n', ' ')
@@ -47,9 +57,9 @@ def main():
                 title = title.replace('\"', '')
                 body = body.replace('\"', '')
 
-                movies_list.append({'title':title, 'body':body})
-                with open('movies.json', 'w', encoding='utf-8') as f:
-                    json.dump(movies_list, f, ensure_ascii=False, indent=2)
+                post_list.append({'title':title, 'body':body})
+                with open(f'{sub}.json', 'w', encoding='utf-8') as f:
+                    json.dump(post_list, f, ensure_ascii=False, indent=2)
 
                 result_cnt += 1
                 print(f'----------Current_dataset_num: {result_cnt}/{dataset_size}----------')
